d81bb136b68554f2bcc83a9174123b859f6aaef9
saiBhandarwad <s333bhandarwad@gmail.com>
2026-07-26 16:32:25 +0530

    production ready changes

--- server/utils/cookie.js
+++ server/utils/cookie.js
@@ -8,7 +8,7 @@ const setRefreshCookie = (res, refreshToken) => {
 
         secure:process.env.NODE_ENV==="production",
 
-        sameSite:"strict",
+        sameSite:"none",
 
         maxAge:7*24*60*60*1000
 
